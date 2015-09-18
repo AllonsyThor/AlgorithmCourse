ffd2cd4ce2fde09ba5d79d31771b0b67a804f66e
AllonsyThor <kt.kllhr@gmail.com>
2015-09-18 16:52:46 -0700

    Queue API

---
+++ QueueApi.java
@@ -0,0 +1,11 @@
+public class QueueOfStrings
+
+QueueOfStrings() /* create an empty queue */
+
+void enqueue(String item) /*insert a new string onto queue */
+
+String dequeue() /* remomve & return the string least recently added */
+
+boolean isEmpty() /* is the queue empty? */
+
+int size() /* number of strings on the queue */
