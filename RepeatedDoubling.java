d41f662fead904e87b9caf28cccae6e57d061097
AllonsyThor <kt.kllhr@gmail.com>
2015-09-18 14:17:41 -0700

    resizing-array implementation repeated doubling

---
+++ RepeatedDoubling.java
@@ -0,0 +1,16 @@
+public ResizingArrayStackOfStrings()
+{ s = new String[1]; }
+
+public void push(String item)
+{
+	if (N == s.length) resize(2 * s.length);
+	s[N++] = item;
+}
+
+private void resize(int capacity)
+{
+	String[] copy = new String[capacity];
+	for (int i = 0; i < N; i++)
+		copy[i] = s[i];
+	s = copy;
+}
