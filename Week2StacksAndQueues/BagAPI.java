7f023506b0d9be74a3379d70d30a84e2c6be9b94
AllonsyThor <kt.kllhr@gmail.com>
2015-09-19 10:56:09 -0700

    Bag API

---
+++ Week2StacksAndQueues/BagAPI.java
@@ -0,0 +1,6 @@
+public class Bag<Item> implements Iterable<Item>
+
+		Bag() 				/* create an empty bag */
+	void add(Item x)		/* insert a new item onto bag */
+	int size()				/* number of items in bag */
+Iterable<Item> iterator()	/* iterator for all items in bag */
