498e11e9545e11e002da693cccbc66b4d69c1ff3
AllonsyThor <kt.kllhr@gmail.com>
2015-09-22 13:04:25 -0700

    Bottom-up mergesort

--- Week3Mergesort/BottomUpMergesort.java
+++ Week3Mergesort/BottomUpMergesort.java
@@ -1,5 +1,7 @@
-public class Merge
+public class MergeBU
 {
+	private static Comparable[] aux;
+
 	private static void merge(Comparable[] a, Comparable[] aux, int lo, int mid, int hi)
 	{
 		assert isSorted(a, lo, mid); // precondition: a[lo..mid] sorted
@@ -17,19 +19,13 @@ public class Merge
 			else                           a[k] = aux[i++];
 		}
 	}
-	
-	private static void sort(Comparable[] a, Comparable[] aux, int lo, int mid, int hi)
-	{
-		if (hi <= lo) return;
-		int mid = lo + (hi - lo)/2;
-		sort (a, aux, lo, mid);
-		sort (a, aux, mid+1, hi);
-		merge(a, aux, lo, mid, hi);
-	}
 
 	public static void sort(Comparable[] a)
 	{
-		aux = new Comparable[a.length];
-		sort(a, aux, 0, a.length - 1);
+		int N = a.length;
+		aux = new Comparable[N];
+		for (int sz = 1; sz < N; sz = sz+sz)
+			for (int lo= 0; lo < N-sz; lo += sz+sz)
+				merge(a, lo, lo+sz-1, Math.min(lo+sz+sz-1, N-1));
 	}
 }
