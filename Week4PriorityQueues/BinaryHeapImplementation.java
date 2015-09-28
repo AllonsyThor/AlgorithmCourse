11ea4174c0bfab141b58c115a703c73a4147bd17
AllonsyThor <kt.kllhr@gmail.com>
2015-09-28 10:24:46 -0700

    added missing code to binary heap Java implementation

--- Week4PriorityQueues/BinaryHeapImplementation.java
+++ Week4PriorityQueues/BinaryHeapImplementation.java
@@ -18,6 +18,19 @@ public class MaxPQ<Key extends Comparable<Key>>
 		return max;
 	}
 
+	private void swim(int k)
+	private void sink(int k)
+	{
+		while (2*k <= N)
+		{
+			int j = 2*k;
+			if (j < N && less(j, j+1)) j++;
+			if (!less(k, j)) break;
+			exch(k, j);
+			k = j;
+		}
+	}
+	
 	private boolean less(int i, int j)
 	{ return pq[i].compareTo(pq[j]) < 0; }
 	private void exch(int i, int j)
