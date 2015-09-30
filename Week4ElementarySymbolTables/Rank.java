3b27040dbeaf3f2ad7cdc235c86c89cac51f974a
AllonsyThor <kt.kllhr@gmail.com>
2015-09-29 17:55:16 -0700

    rank

---
+++ Week4ElementarySymbolTables/Rank.java
@@ -0,0 +1,11 @@
+public int rank(Key key)
+{ return rank(key, root); }
+
+private int rank(Key key, Node x)
+{
+	if (x == null) return 0;
+	int cmp = key.compareTo(x.key);
+	if      (cmp < 0) return rank(key, x.left);
+	else if (cmp > 0) return 1 + size(x.left) + rank(key, x.right);
+	else			  return size(x.left);
+}
